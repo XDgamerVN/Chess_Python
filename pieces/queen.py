da5d089e65f3626cf0ae2f5d6f5a62300e2ed96f
XDgamerVN <131602164+XDgamerVN@users.noreply.github.com>
2024-08-19 22:38:42 +0700

    Delete pieces/queen.py

--- pieces/queen.py
+++
@@ -1,9 +0,0 @@
-from pieces.rook import check_rook
-from pieces.bishop import check_bishop
-
-def check_queen(position, color):
-    moves_list = check_bishop(position, color)
-    second_list = check_rook(position, color)
-    for i in range(len(second_list)):
-        moves_list.append(second_list[i])
-    return moves_list
