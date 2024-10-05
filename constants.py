b89be42d8891641dd94ca2f6787d828dfd1d2e31
Văn Dũng <131602164+XDgamerVN@users.noreply.github.com>
2024-10-05 14:44:55 +0700

    Update and rename Constants.py to constants.py

---
+++ constants.py
@@ -0,0 +1,17 @@
+import pygame
+
+WIDTH = 1440
+HEIGHT = 810
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+images = {}
+captures_images = {}
+promote_images = {}
+
+COLOR_A = ['#ebecd0', '#739552']
+COLOR_Z = ['#739552', '#ebecd0']
+COLOR_SCREEN = 'grey22'
+COLOR_GAME = 'wheat4'
+
