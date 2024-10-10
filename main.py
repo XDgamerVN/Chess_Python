497eeee312b5b524f1053594c8e1fe672ca4db89
Văn Dũng <131602164+XDgamerVN@users.noreply.github.com>
2024-10-10 19:25:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from interface import main_menu
+from decryption import *
 import os
 import sys
 import pygame
@@ -7,7 +8,7 @@ pygame.init()
 
 # Hàm để lấy đường dẫn tài nguyên khi đóng gói bằng PyInstaller
 def resource_path(relative_path):
-    """ Lấy đường dẫn tới file tài nguyên khi chạy từ file .exe """
+    """Lấy đường dẫn tới file tài nguyên khi chạy từ file"""
     try:
         base_path = sys._MEIPASS  # PyInstaller trích xuất tài nguyên tạm thời
     except AttributeError:
@@ -15,14 +16,9 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
-# Lấy đường dẫn tới file icon
-icon = pygame.image.load(resource_path(os.path.join('images', 'logo.png')))
-
-# Thiết lập icon cho cửa sổ
+icon = load_image('logo', (256, 256))
 pygame.display.set_icon(icon)
-
-# Thiết lập tiêu đề cửa sổ
-pygame.display.set_caption('Chess_Python by Nguyen Le Van Dung')
+pygame.display.set_caption('Chess_Python by Nguyen Le Van Dung - Satoru Gojo Edition')
 
 if __name__ == '__main__':
     main_menu()
